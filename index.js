03039555094ce8d63deda22b966355067c8222e3
bhagya <bhagya818@gmail.com>
2023-07-18 12:59:21 +0530

    newcode

--- index.js
+++ index.js
@@ -22,47 +22,91 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 // Enter new invoice details
-app.post("/", async (req, res) => {
-    const { date, number, amount, id } = req.body;
+// app.post("/", async (req, res) => {
+//     const { date, number, amount, id } = req.body;
+//     try {
+//         const existingInvoice = await Invoice.findOne({ number });
+//         if (existingInvoice) {
+//             const existingFinancialYear = getFinancialYear(existingInvoice.date);
+//             const newFinancialYear = getFinancialYear(date);
+//             console.log('ex')
+//             console.log(newFinancialYear,'new')
+//             if (existingFinancialYear === newFinancialYear) {
+//                 return res.status(400).json({ error: 'Invoice number already exists for the same financial year.' });
+//             }
+//             else{
+//                 res.json(invoices)
+//             }
+//         }
+//         const previousInvoice = await Invoice.findOne({ number: number - 1 });
+//         const nextInvoice = await Invoice.findOne({ number: number + 1 });
+
+//         if (
+//             (previousInvoice && date < previousInvoice.date) ||
+//             (nextInvoice && date > nextInvoice.date)
+//         ) {
+//             return res.status(400).json({ error: 'Invoice date is not within the valid range.' });
+//         }
+
+//         let newInvoice = new Invoice({
+//             date,
+//             number,
+//             amount,
+//             id,
+//             FinancialYear: getFinancialYear(date)
+//         });
+//         await newInvoice.save();
+//         const invoices = await Invoice.find();
+//         res.json(invoices);
+//     } catch (err) {
+//         console.log(err.message);
+//         res.status(500).json({ error: 'Internal server error.' });
+//     }
+// });
+
+app.post('/invoices', async (req, res) => {
     try {
+        const { date, number, amount } = req.body;
+
+        // Check if the invoice number already exists for the same financial year
         const existingInvoice = await Invoice.findOne({ number });
         if (existingInvoice) {
-            const existingFinancialYear = getFinancialYear(existingInvoice.date);
+            const existingFinancialYear = existingInvoice.financialYear;
             const newFinancialYear = getFinancialYear(date);
-            console.log('ex')
-            console.log(newFinancialYear,'new')
+
             if (existingFinancialYear === newFinancialYear) {
                 return res.status(400).json({ error: 'Invoice number already exists for the same financial year.' });
             }
-            else{
-                res.json(invoices)
-            }
         }
+
+        // Check if the invoice date is within the valid range
         const previousInvoice = await Invoice.findOne({ number: number - 1 });
         const nextInvoice = await Invoice.findOne({ number: number + 1 });
 
         if (
-            (previousInvoice && date < previousInvoice.date) ||
-            (nextInvoice && date > nextInvoice.date)
+            (previousInvoice && date <= previousInvoice.date) ||
+            (nextInvoice && date >= nextInvoice.date)
         ) {
             return res.status(400).json({ error: 'Invoice date is not within the valid range.' });
         }
 
-        let newInvoice = new Invoice({
+        // Create the new invoice
+        const financialYear = getFinancialYear(date);
+        const newInvoice = new Invoice({
             date,
             number,
             amount,
-            id,
-            FinancialYear: getFinancialYear(date)
+            financialYear,
         });
         await newInvoice.save();
-        const invoices = await Invoice.find();
-        res.json(invoices);
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).json({ error: 'Internal server error.' });
+
+        return res.status(201).json(newInvoice);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error' });
     }
 });
+
 
 
 // Get all invoices stored in the db
@@ -119,13 +163,20 @@ app.delete('/:number', async (req, res) => {
     }
 });
 
-function getFinancialYear(date) {
-    const dates = new Date(date);
-    const currentYear = dates.getFullYear();
-    const nextYear = currentYear + 1;
-    const fiscalYear = `${currentYear}-${nextYear.toString().slice(-2)}`;
-    return fiscalYear;
-}
+// function getFinancialYear(date) {
+//     const dates = new Date(date);
+//     const currentYear = dates.getFullYear();
+//     const nextYear = currentYear + 1;
+//     const fiscalYear = `${currentYear}-${nextYear.toString().slice(-2)}`;
+//     return fiscalYear;
+// }
+const getFinancialYear = (date) => {
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const financialYearStart = month >= 3 ? year : year - 1;
+    const financialYearEnd = financialYearStart + 1;
+    return `${financialYearStart}-${financialYearEnd}`;
+};
 
 
 function getExtingYear(date) {
